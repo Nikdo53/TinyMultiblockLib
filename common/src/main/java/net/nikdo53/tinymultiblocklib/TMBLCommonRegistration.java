23c914a93c1d69b92643ea2935295bc6b9e6bba3
Nikdo53 <usivatel@gmail.com>
2026-01-06 22:52:27 +0100

    MultiBlockEntity without registration

---
+++ common/src/main/java/net/nikdo53/tinymultiblocklib/TMBLCommonRegistration.java
@@ -0,0 +1,11 @@
+package net.nikdo53.tinymultiblocklib;
+
+public class TMBLCommonRegistration {
+    public static final class Blocks{
+
+    }
+
+    public static final class BlockEntities{
+        
+    }
+}
