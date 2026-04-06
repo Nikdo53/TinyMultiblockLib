e1266b060a49c6c22390f791a830bdaa57e61191
Nikdo53 <usivatel@gmail.com>
2026-04-06 12:27:14 +0200

    Fix fabric again

--- fabric/src/main/java/net/nikdo53/tinymultiblocklib/platform/FabricRegistration.java
+++ fabric/src/main/java/net/nikdo53/tinymultiblocklib/platform/FabricRegistration.java
@@ -1,5 +1,6 @@
 package net.nikdo53.tinymultiblocklib.platform;
 
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -9,9 +10,11 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.nikdo53.tinymultiblocklib.Constants;
+import net.nikdo53.tinymultiblocklib.mixin.BlockEntityTypeAccessor;
 import net.nikdo53.tinymultiblocklib.platform.services.IRegistrationUtils;
 import net.nikdo53.tinymultiblocklib.test.TestBlockItem;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.function.BiFunction;
 import java.util.function.Supplier;
@@ -21,7 +24,7 @@ public class FabricRegistration implements IRegistrationUtils {
 
     @Override
     public <T extends BlockEntity> Supplier<BlockEntityType<T>> registerBlockEntity(String name, BiFunction<BlockPos, BlockState, T> function, Set<Block> blocks) {
-        BlockEntityType<T> registered = Registry.register(BuiltInRegistries.BLOCK_ENTITY_TYPE, name, BlockEntityType.Builder.of(function::apply, blocks.toArray(new Block[0])).build(null));
+        BlockEntityType<T> registered = Registry.register(BuiltInRegistries.BLOCK_ENTITY_TYPE, name, FabricBlockEntityTypeBuilder.create(function::apply, blocks.toArray(new Block[0])).build(null));
         return () -> registered;
     }
 
@@ -40,6 +43,10 @@ public class FabricRegistration implements IRegistrationUtils {
 
     @Override
     public  <T extends BlockEntity> void addSupportedBEBlock(Supplier<BlockEntityType<T>> blockEntityType, Block block){
-        blockEntityType.get().validBlocks.add(block);
+        BlockEntityTypeAccessor accessor = (BlockEntityTypeAccessor) blockEntityType.get();
+        Set<Block> blocks = new HashSet<>(accessor.tinymultiblocklib$getValidBlocks());
+        blocks.add(block);
+
+        accessor.tinymultiblocklib$setValidBlocks(blocks);
     }
 }
