2c87fa73481c03a49bdf03e2f8a055e95ed4b2fd
Nikdo53 <usivatel@gmail.com>
2026-08-22 19:31:23 +0200

    Common config

---
+++ common/src/main/java/net/nikdo53/tinymultiblocklib/config/TMBLClientConfig.java
@@ -0,0 +1,57 @@
+package net.nikdo53.tinymultiblocklib.config;
+
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.Identifier;
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TMBLClientConfig {
+    public static final ModConfigSpec CLIENT_CONFIG;
+    public static final ModConfigSpec.BooleanValue DISABLE_MULTIBLOCK_PREVIEWS;
+    public static final ModConfigSpec.BooleanValue PREVIEWS_FOR_EVERYTHING;
+    public static final ModConfigSpec.ConfigValue<List<? extends String>> PREVIEW_WHITELIST;
+    public static final ModConfigSpec.ConfigValue<List<? extends String>> PREVIEW_BLACKLIST;
+
+
+
+    static {
+        ModConfigSpec.Builder builder = new ModConfigSpec.Builder();
+
+        DISABLE_MULTIBLOCK_PREVIEWS = builder
+                .comment("Disables ghost previews when trying to place a multiblock")
+                .translation("tinymultiblocklib.configuration.disable_multiblock_previews")
+                .define("Disable Multiblock Previews", false);
+
+        PREVIEWS_FOR_EVERYTHING = builder
+                .comment("Enables previews for everything, Used for debugging")
+                .translation("tinymultiblocklib.configuration.previews_for_everything")
+                .define("Previews for Everything", false);
+
+        PREVIEW_WHITELIST = builder
+                .comment("List of blocks for which to show previews")
+                .translation("tinymultiblocklib.configuration.preview_whitelist")
+                .defineList("Preview Whitelist", new ArrayList<>(), () -> "minecraft:dirt", TMBLClientConfig::validateBlockName);
+
+        PREVIEW_BLACKLIST = builder
+                .comment("List of blocks for which to hide previews")
+                .translation("tinymultiblocklib.configuration.preview_blacklist")
+                .defineList("Preview Blacklist", new ArrayList<>(), () -> "minecraft:dirt", TMBLClientConfig::validateBlockName);
+
+
+
+        CLIENT_CONFIG = builder.build();
+    }
+
+    public static boolean validateBlockName(final Object object){
+        if (object instanceof String s){
+            Identifier parse = Identifier.tryParse(s);
+            if (parse != null){
+               return BuiltInRegistries.BLOCK.containsKey(parse);
+            }
+        }
+        return false;
+    }
+}
--- neoforge/src/main/java/net/nikdo53/tinymultiblocklib/client/TMBLClientEvents.java
+++ neoforge/src/main/java/net/nikdo53/tinymultiblocklib/client/TMBLClientEvents.java
@@ -2,32 +2,26 @@ package net.nikdo53.tinymultiblocklib.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.DeltaTracker;
-import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
-import net.neoforged.neoforge.client.event.ExtractLevelRenderStateEvent;
-import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
 import net.neoforged.neoforge.client.event.SubmitCustomGeometryEvent;
 import net.nikdo53.tinymultiblocklib.Constants;
 import net.nikdo53.tinymultiblocklib.client.ghost.GhostRenderer;
-import net.nikdo53.tinymultiblocklib.platform.NeoForgePlatformHelper;
+import net.nikdo53.tinymultiblocklib.config.TMBLClientConfig;
 
 @EventBusSubscriber(modid = Constants.MOD_ID, value = Dist.CLIENT)
 public class TMBLClientEvents {
 
     @SubscribeEvent
     public static void renderLevelStage(SubmitCustomGeometryEvent event){
-        if (!TMBLClientConfig.DISABLE_MULTIBLOCK_PREVIEWS.get()){
-            MultiblockPreviewRenderer.tryRenderMultiblockPreviews(
-                    DeltaTracker.ONE.getGameTimeDeltaPartialTick(true),
-                    event.getLevelRenderState().cameraRenderState,
-                    new PoseStack(),
-                    event.getSubmitNodeCollector()
-            );
-        }
+        MultiblockPreviewRenderer.tryRenderMultiblockPreviews(
+                DeltaTracker.ONE.getGameTimeDeltaPartialTick(true),
+                event.getLevelRenderState().cameraRenderState,
+                new PoseStack(),
+                event.getSubmitNodeCollector()
+        );
 
         GhostRenderer.renderAll(
                 DeltaTracker.ONE.getGameTimeDeltaPartialTick(true),
